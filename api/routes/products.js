b0863a717178cd57a36e44371976bb8d13ddd020
Kosal Mr. Thoeurn <Kosal@kosal.udaya.lan>
2019-02-26 00:41:55 +0700

    create route product and order

---
+++ api/routes/products.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/' , (req , res , next)=>{
+    res.status(200).json({
+        message : 'Handling GET requests to /products'
+    })
+});
+
+
+router.post('/' , (req , res , next)=>{
+    res.status(201).json({
+        message : 'Handling GET post to /products'
+    })
+});
+
+router.get('/:productId', (req, res , next) =>{
+    const id = req.params.productId;
+    if (id === 'specail'){
+        res.status(200).json({
+            message : 'You discoverd the specail ID',
+            id : id
+        });
+    }
+    else{
+        res.status(200).json({
+            message: 'You passed an ID '
+        })
+    }
+})
+
+
+router.patch('/:productId', (req, res , next) =>{
+    res.status(201).json({
+        message: 'Update product'
+    });
+});
+
+router.delete('/:productId', (req, res , next) =>{
+    res.status(200).json({
+        message: 'Delete product'
+    });
+});
+
+
+
+
+module.exports  = router;
